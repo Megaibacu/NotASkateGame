e92775b5ea27ab796dddef93ad25081e22bbc07e
Megaibacu <megaibacu888@gmail.com>
2023-05-14 17:31:11 +0200

    Camera Update

--- Assets/Scripts/Player/Camera/ThirdPersonCam.cs
+++ Assets/Scripts/Player/Camera/ThirdPersonCam.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class ThirdPersonCam : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class ThirdPersonCam : MonoBehaviour
     public Rigidbody rb;
     StateChange state;
     public PlayerInput _playerInput;
+    public CinemachineFreeLook cam;
+    public float camCd;
+    public float camCdT;
 
     public float rotationSpeed;
 
@@ -27,6 +31,7 @@ public class ThirdPersonCam : MonoBehaviour
     {
         if(state.state == States.parkour && !player.GetComponent<PlayerMovement>().wallrunning)
         {
+            cam.Priority = 11;
             //  rotate orientation
             Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
             orientation.forward = viewDir.normalized;
@@ -39,6 +44,25 @@ public class ThirdPersonCam : MonoBehaviour
             if (inputDir != Vector3.zero)
                 playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
         }
-                    
+
+         if(state.state == States.skating)
+        {
+            if (Input.GetAxis("Mouse X") == 0 && Input.GetAxis("Mouse Y") == 0)
+            {
+                if(camCdT > 0)
+                {
+                    camCdT -= Time.deltaTime;
+                }
+                else
+                cam.Priority = 9;
+
+            }
+            else
+            {
+                camCdT = camCd;
+                cam.Priority = 11;
+            }
+        }
     }
+
 }
